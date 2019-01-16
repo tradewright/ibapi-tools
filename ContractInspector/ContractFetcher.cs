04f9b095be354b0402c093c3bcbfa04ef78723b4
Richard L King <rlking@aultan.com>
2019-01-16 11:57:08 +0000

    Update licence headers

--- ContractInspector/ContractFetcher.cs
+++ ContractInspector/ContractFetcher.cs
@@ -2,7 +2,7 @@
 
 // The MIT License (MIT)
 //
-// Copyright (c) 2018 Richard L King (TradeWright Software Systems)
+// Copyright (c) 2019 Richard L King (TradeWright Software Systems)
 // 
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
